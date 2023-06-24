65378a5a9ee9d73bf6bd9d8aa5200b351204c5d0
anyounghwa <e89cokr@naver.com>
2023-06-24 17:22:32 +0900

    연결할 DB 정보입력 수정

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 
 // 연결할 DB 정보입력
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '111111',
+    host: 'database-1.chqupvl9apyw.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'dmcom4544**',
     database: 'comicbook',
     port: '3306',
 });
